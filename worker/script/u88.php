a4b43c1870156482e2086852bc0b5c893f3f561e
太年轻 <tthd@163.com>
2017-01-06 16:31:33 +0800

    爬虫

--- worker/script/u88.php
+++ worker/script/u88.php
@@ -1,28 +1,31 @@
 <?php 
 
+// 抓取u88新闻
+
 require '../../src/main.php';
 
 $configs = [
-    'name' => 'u88',
+    'name'          =>  'u88',
     // 'daemonize' => true,
-    'worker_num' => 10,
-    'interval'  =>  10,
-    'domains' => [
+    'worker_num'    =>  10,
+    'interval'      =>  10,
+    'log_file'      =>  'u88',
+    'domains'       => [
         'u88.cn',
         'www.u88.cn'
     ],
-    'entry_urls' => [
+    'entry_urls'    =>  [
         'http://www.u88.cn/'
     ],
-    'content_url_regexes' => [
+    'content_url_regexes'   =>  [
         "http://www.u88.cn/news/[a-z]+/[a-z]+/\d+\.html\.*",
     ],
-    'max_try' => 5,
+    'max_try'   => 5,
     // 'export' => [
     //     'type' => 'csv',
     //     'file' => __DIR__ . '/u88.csv',
     // ],
-    'export' => [
+    'export'    =>  [
         'type'  => 'sql',
         'file'  => __DIR__ . '/u88.sql',
         'table' => 'content',
@@ -31,16 +34,28 @@ $configs = [
     //     'type' => 'db', 
     //     'table' => 'content',
     // ],
-    'fields' => [
+    'fields'    => [
         [
-            'name' => "article_content",
-            'selector' => '/html/body/div[1]/div[2]/div[1]/div[1]/div[2]',
-            'required' => true,
+            'name'      =>  "article_content",
+            'selector'  =>  '/html/body/div[1]/div[2]/div[1]/div[1]/div[2]',
+            'required'  =>  true,
         ]
     ],
 ];
 
 $craw = new crawler\core\Crawler($configs);
+
+// 支持的回调
+// public $request_status_code     =   null;
+// public $html_download_page      =   null;
+// public $html_entry_page         =   null;
+// public $html_list_page          =   null;
+// public $html_content_page       =   null;
+// public $html_download_attached_page =   null;
+// public $html_handle_img         =   null;
+// public $html_extract_field      =   null;
+// public $html_extract_page       =   null;
+    
 // $craw->html_extract_field = function($fieldname, $data, $page) {
 //     if ($fieldname === 'depth') {
 //         return $page['request']['depth'];
@@ -48,4 +63,5 @@ $craw = new crawler\core\Crawler($configs);
 
 //     return $data;
 // };
+
 $craw->run();
